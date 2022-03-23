bc9bee1f4567b5e7a7298c6b6672b131d5785fd6
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-23 17:50:46 +0530

    Mortie

--- app/src/main/java/com/example/morty_app/feature_morty/data/remote/dto/CharacterListDto.kt
+++ app/src/main/java/com/example/morty_app/feature_morty/data/remote/dto/CharacterListDto.kt
@@ -1,6 +1,5 @@
 package com.example.morty_app.feature_morty.data.remote.dto
 
-import com.example.morty_app.feature_morty.domain.model.Character
 import com.example.morty_app.feature_morty.domain.model.CharacterList
 
 data class CharacterListDto(
