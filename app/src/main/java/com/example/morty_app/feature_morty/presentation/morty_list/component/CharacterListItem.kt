60b232d8bbad5335b7ae596a4b310070fd50bebe
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-25 09:14:01 +0530

    Mortie

--- app/src/main/java/com/example/morty_app/feature_morty/presentation/morty_list/component/CharacterListItem.kt
+++ app/src/main/java/com/example/morty_app/feature_morty/presentation/morty_list/component/CharacterListItem.kt
@@ -3,11 +3,12 @@ package com.example.morty_app.feature_morty.presentation.morty_list.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,17 +18,29 @@ import com.example.morty_app.feature_morty.domain.model.Character
 @Composable
 fun CharacterListItem(
     character: Character,
+    onItemClick: (Character) -> Unit
 ) {
     Row(modifier = Modifier
         .fillMaxWidth()
+        .clickable { onItemClick(character) }
         .padding(20.dp),
         horizontalArrangement = Arrangement.Start, verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             painter = rememberImagePainter(data = character.image),
             contentDescription = null,
-            modifier = Modifier.size(50.dp)
+            modifier = Modifier
+                .size(50.dp)
+                .clip(RoundedCornerShape(10.dp))
         )
-        Text(text = "${character.name}", style = TextStyle(fontSize = 30.sp))
+        Spacer(modifier = Modifier.padding(10.dp))
+        Column(verticalArrangement = Arrangement.Center) {
+            Text(text = "${character.name}", style = TextStyle(fontSize = 18.sp))
+            Text(
+                text = "${character.species} - ${character.status}",
+                style = TextStyle(fontSize = 15.sp)
+            )
+        }
+
     }
 }
