5b8682a5d634c3bbd92f611d88b6c32b08c7104d
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-25 11:10:57 +0530

    Mortie

--- app/src/main/java/com/example/morty_app/feature_morty/data/remote/dto/CharacterListDto.kt
+++ app/src/main/java/com/example/morty_app/feature_morty/data/remote/dto/CharacterListDto.kt
@@ -1,15 +1,6 @@
 package com.example.morty_app.feature_morty.data.remote.dto
 
-import com.example.morty_app.feature_morty.domain.model.CharacterList
-
 data class CharacterListDto(
     val info: InfoDto,
     val results: List<CharacterDto>
 )
-
-fun CharacterListDto.toCharacterList(): CharacterList{
-    return CharacterList(
-        info = info,
-        results = results
-    )
-}
--- app/src/main/java/com/example/morty_app/feature_morty/domain/model/CharacterList.kt
+++
@@ -1,9 +0,0 @@
-package com.example.morty_app.feature_morty.domain.model
-
-import com.example.morty_app.feature_morty.data.remote.dto.CharacterDto
-import com.example.morty_app.feature_morty.data.remote.dto.InfoDto
-
-data class CharacterList(
-    val info: InfoDto,
-    val results: List<CharacterDto>
-)
