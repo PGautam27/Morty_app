5b3af871bd123175aa2c8bd5c23ddefa29b37d8f
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-29 18:27:56 +0530

    Rick-Mortie

--- app/src/main/java/com/example/morty_app/feature_morty/presentation/morty_list/MortyListViewModel.kt
+++ app/src/main/java/com/example/morty_app/feature_morty/presentation/morty_list/MortyListViewModel.kt
@@ -1,14 +1,14 @@
 package com.example.morty_app.feature_morty.presentation.morty_list
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.*
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.morty_app.feature_morty.domain.model.Character
 import com.example.morty_app.feature_morty.domain.use_cases.get_characters.GetCharactersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
