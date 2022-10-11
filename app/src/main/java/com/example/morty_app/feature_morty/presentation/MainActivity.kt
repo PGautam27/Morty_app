5346eac440685b21dc5fa7339d7cc5bdd49e34ac
Vineet Sen <imvsen02@gmail.com>
2022-10-11 19:03:05 +0530

    Added animation in navigation between the screens

--- app/src/main/java/com/example/morty_app/feature_morty/presentation/MainActivity.kt
+++ app/src/main/java/com/example/morty_app/feature_morty/presentation/MainActivity.kt
@@ -3,32 +3,72 @@ package com.example.morty_app.feature_morty.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.navigation
 import com.example.morty_app.feature_morty.presentation.morty_details.MortyDetailScreen
 import com.example.morty_app.feature_morty.presentation.morty_list.MortyListScreen
 import com.example.morty_app.ui.theme.Morty_appTheme
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import androidx.navigation.NavBackStackEntry
 import dagger.hilt.android.AndroidEntryPoint
 
+@OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Morty_appTheme {
-                val navController = rememberNavController()
-                NavHost(
+                val navController = rememberAnimatedNavController()
+                AnimatedNavHost(
                     navController = navController,
                     startDestination = Screen.MortyScreen.route
                 ) {
                     composable(
-                        route = Screen.MortyScreen.route
+                        route = Screen.MortyScreen.route,
+                        exitTransition = {
+                            slideOutHorizontally(
+                                targetOffsetX = { -1200 },
+                                animationSpec = tween(
+                                    durationMillis = 500,
+                                    easing = FastOutSlowInEasing
+                                )
+                            ) + fadeOut(animationSpec = tween(800))
+                        },
+                        popEnterTransition = {
+                            slideInHorizontally(
+                                initialOffsetX = { -1200 },
+                                animationSpec = tween(
+                                    durationMillis = 500
+                                )
+                            ) + fadeIn(animationSpec = tween(800))
+                        }
                     ) {
                         MortyListScreen(navController = navController)
                     }
                     composable(
-                        route = Screen.MortyDetailScreen.route + "/{characterId}"
+                        route = Screen.MortyDetailScreen.route + "/{characterId}",
+                        enterTransition = {
+                            slideInHorizontally(
+                                initialOffsetX = { 1200 },
+                                animationSpec = tween(
+                                    durationMillis = 500
+                                )
+                            ) + fadeIn(animationSpec = tween(800))
+                        },
+                        popExitTransition = {
+                            slideOutHorizontally(
+                                targetOffsetX = { 1200 },
+                                animationSpec = tween(
+                                    durationMillis = 500,
+                                    easing = FastOutSlowInEasing
+                                )
+                            ) + fadeOut(animationSpec = tween(800))
+                        }
                     ) {
                         MortyDetailScreen(navController = navController)
                     }
